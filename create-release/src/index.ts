eaf1816d239c1195bb163b2a507dce28ba7a30c8
Carlos Ramos <57056947+CarlosIRamosV@users.noreply.github.com>
2023-12-23 19:01:20 -0600

    test

--- create-release/src/index.ts
+++ create-release/src/index.ts
@@ -6,15 +6,7 @@ import * as core from '@actions/core'
  */
 export async function run(): Promise<void> {
   try {
-    // Print a message to the info stream.
-    core.info('Hello, world!')
-    core.debug('Debugging')
-    core.warning('Warning')
-    console.log('Hello, world!')
-    core.error('Error')
-    core.setFailed('Failure')
-    core.setCommandEcho(true)
-    console.log('Hello, world!')
+
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
