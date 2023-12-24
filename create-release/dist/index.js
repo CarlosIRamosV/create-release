9f89dbc5e7107dc7a5b8fde46f5e4a48d5cb1fb6
Carlos Ramos <57056947+CarlosIRamosV@users.noreply.github.com>
2023-12-23 19:04:53 -0600

    test

--- create-release/dist/index.js
+++ create-release/dist/index.js
@@ -24739,15 +24739,7 @@ const core = __importStar(__nccwpck_require__(3722));
  */
 async function run() {
     try {
-        // Print a message to the info stream.
-        core.info('Hello, world!');
-        core.debug('Debugging');
-        core.warning('Warning');
-        console.log('Hello, world!');
-        core.error('Error');
-        core.setFailed('Failure');
-        core.setCommandEcho(true);
-        console.log('Hello, world!');
+        core.debug('Hello world!');
     }
     catch (error) {
         if (error instanceof Error)
--- create-release/src/index.ts
+++ create-release/src/index.ts
@@ -6,7 +6,7 @@ import * as core from '@actions/core'
  */
 export async function run(): Promise<void> {
   try {
-
+    core.debug('Hello world!')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
